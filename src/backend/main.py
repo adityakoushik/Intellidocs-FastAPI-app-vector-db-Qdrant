8837c3ba76d45ad3b833a6eb7d0e15e6f9f15e0b
adityakoushik <duttakoushik760@gmail.com>
2026-09-03 06:34:47 +0530

    Enhance documents collection functionality with detailed comments and implement seed_documents endpoint for inserting data

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from backend.db.qdrant import qdrant_client
-from qdrant_client.models import Distance, VectorParams
+from qdrant_client.models import Distance, VectorParams, PointStruct
 
 # Now here creating object of FastAPI class
 app = FastAPI(
@@ -48,7 +48,9 @@ def create_documents_collection():
     qdrant_client.create_collection(
         collection_name="documents",
         # VectorParams is decided that how the collection of vectors will be stored in the Qdrant database. It has two parameters size and distance. 
-        # Size is the dimension of the vector and distance is the distance metric used to calculate the similarity between vectors.
+        # Size is the dimension of the vector which is 3 , means accept only 3 dimensional vector in the collection i.e. [0.2, 0.5, 0.9]
+        # and distance is the distance metric used to calculate the similarity between vectors.
+        # And Similarity method = Cosine, so its Cosine similarity/distance based search
         vectors_config = VectorParams(
             size = 3,
             distance = Distance.COSINE
@@ -57,3 +59,75 @@ def create_documents_collection():
     return {
         "message": "Documents collection created successfully"
     }
+
+@app.post("/documents/seed")
+def seed_documents():
+    # points resembles the data that we want to insert into the Qdrant collection. Each point is represented as a PointStruct object, which contains an id, a vector, and optional payload data.
+    points = [
+        # these vectors are 3 dimensional manual vector for learning purpose, in real world we will use embedding model to get the vector representation of the text.
+        PointStruct(
+            id=1,
+            vector=[1.0, 0.0, 0.0],
+            payload={
+                "text": "Employees get 12 casual leaves per year."
+            }
+        ),
+        PointStruct(
+            id=2,
+            vector=[0.0, 1.0, 0.0],
+            payload={
+                "text": "Docker packages applications into containers."
+            }
+        ),
+        PointStruct(
+            id=3,
+            vector=[0.0, 0.0, 1.0],
+            payload={
+                "text": "React is used for building user interfaces."
+            }
+        ),
+    ]
+    # upsert is responsible for inserting or updating the points in the Qdrant collection. If a point with the same id already exists, it will be updated; otherwise, a new point will be inserted.
+    qdrant_client.upsert(
+        collection_name="documents",
+        points=points
+    )
+    
+    return {
+        "message": "Documents inserted successfully"
+    }
+    
+@app.get("/documents/search")
+def search_documents():
+    # Example query vector for searching
+    # Stored: [1.0, 0.0, 0.0]
+    # Query : [0.9, 0.1, 0.0]
+    query_vector = [0.9, 0.1, 0.0]
+    
+    # This below means Qdrant, find the points most similar to this query vector.
+    results = qdrant_client.query_points(
+        collection_name="documents",
+        # query_vector = This means Qdrant will compare this vector with the stored vectors.
+        query=query_vector,
+        limit=3
+    )
+    
+    return {
+        # "results": [
+        #     {
+        #         "id": point.id,
+        #         "score": point.score,
+        #         "payload": point.payload
+        #     }
+        #     for point in results.points
+        # ]
+        "results": [
+            {
+                "id": point.id,
+                "score": point.score,
+                "payload": point.payload
+            }
+            for point in results.points
+        ]
+    }
+    
